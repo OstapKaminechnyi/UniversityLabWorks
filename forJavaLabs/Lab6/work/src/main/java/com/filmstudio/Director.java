07578f731a40103cabb0db28db8fac7ae2d0fa75
OstapKaminechnyi <ostap1599@gmail.com>
2018-03-08 12:06:22 +0200

    test coverage is 100%
    Lab6

---
+++ forJavaLabs/Lab6/work/src/main/java/com/filmstudio/Director.java
@@ -0,0 +1,27 @@
+package com.filmstudio;
+
+import model.enums.FilmGenre;
+
+public class Director extends Worker {
+    private String scenario;
+
+    public Director(final String pScenario, final String occupationName, final String firstName,
+                    final String lastName, final int age, final int salaryPerHour,
+                    final FilmGenre genre, final int yearExperience) {
+        super(occupationName, firstName, lastName, age, salaryPerHour, genre, yearExperience);
+        this.scenario = pScenario;
+    }
+
+
+    public Director() {
+    }
+
+    public final String getScenario() {
+        return scenario;
+    }
+
+    public final void setScenario(final String pScenario) {
+        this.scenario = pScenario;
+    }
+
+}
---
+++ forJavaLabs/Lab6/work/src/main/java/com/filmstudio/Worker.java
@@ -0,0 +1,104 @@
+package com.filmstudio;
+
+import model.enums.FilmGenre;
+
+
+public class Worker {
+    private String occupationName;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int salaryPerHour;
+    private FilmGenre genre;
+    private int yearExperience;
+
+    public final String getOccupationName() {
+        return occupationName;
+    }
+
+
+    public final void setOccupationName(final String pOccupationName) {
+        this.occupationName = pOccupationName;
+    }
+
+
+    public final FilmGenre getGenre() {
+        return genre;
+    }
+
+
+    public Worker() {
+    }
+
+
+    public Worker(final String pOccupationName, final String pFirstName,
+                  final String pLastName, final int pAge, final int pSalaryPerHour,
+                  final FilmGenre pGenre, final int pYearExperience) {
+        this.firstName = pFirstName;
+        this.lastName = pLastName;
+        this.age = pAge;
+        this.salaryPerHour = pSalaryPerHour;
+        this.genre = pGenre;
+        this.yearExperience = pYearExperience;
+        this.occupationName = pOccupationName;
+    }
+
+
+    public final String getFirstName() {
+        return firstName;
+    }
+
+
+    public final void setFirstName(final String pFirstName) {
+        this.firstName = pFirstName;
+    }
+
+
+    public final String getLastName() {
+        return lastName;
+    }
+
+
+    public final void setLastName(final String pLastName) {
+        this.lastName = pLastName;
+    }
+
+
+    public final int getAge() {
+        return age;
+    }
+
+
+    public final void setAge(final int pAge) {
+        this.age = pAge;
+    }
+
+
+    public final int getSalaryPerHour() {
+        return salaryPerHour;
+    }
+
+
+    public final void setSalaryPerHour(final int pSalaryPerHour) {
+        this.salaryPerHour = pSalaryPerHour;
+    }
+
+
+    public final int getYearExperience() {
+        return yearExperience;
+    }
+
+
+    public final void setYearExperience(final int pYearExperience) {
+        this.yearExperience = pYearExperience;
+    }
+
+    @Override
+    public final String toString() {
+        return "Worker [occupation = " + occupationName + ", first Name = "
+                + firstName + ", last Name = " + lastName + ",age = "
+                + age + ", salary per hour = " + salaryPerHour
+               + ",film genre can play = " + genre + ", year Experience = "
+              +  yearExperience + "]" + "\n";
+    }
+}
