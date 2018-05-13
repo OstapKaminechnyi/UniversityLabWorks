3c60db72957237576f0d70d3b1fa08e32c38cc39
OstapKaminechnyi <ostap1599@gmail.com>
2018-05-13 13:50:17 +0300

    fixed intendation

--- forJavaLabs/Lab1/CartonOfMilk.java
+++ forJavaLabs/Lab1/CartonOfMilk.java
@@ -1,43 +1,48 @@
 
 public class CartonOfMilk {
 
-	    private String name = "Untitled";
-	    private double price;
-	    private String color;
-	    private double capacity;
-	    private int number;
-	    public static int generalNumber = 0;
+    private String name = "Untitled";
+    private double price;
+    private String color;
+    private double capacity;
+    private int number;
+    public static int generalNumber = 0;
 
-    public CartonOfMilk () {
-	
-}
+    public CartonOfMilk() {
 
-    public CartonOfMilk (String name, double price, String color, double capacity) {
+    }
+
+    public CartonOfMilk(String name, double price, String color, double capacity) {
         setName(name);
-        setPrice (price);
-        setColor (color);
-        setCapacity (capacity);
+        setPrice(price);
+        setColor(color);
+        setCapacity(capacity);
 
-}
-    public CartonOfMilk (String name, double price, String color, double capacity, int number ) {
+    }
+
+    public CartonOfMilk(String name, double price, String color, double capacity, int number) {
         setName(name);
-        setPrice (price);
-        setColor (color);
-        setCapacity (capacity);
-        setNumber (number);
-}
+        setPrice(price);
+        setColor(color);
+        setCapacity(capacity);
+        setNumber(number);
+    }
+
     @Override
     public String toString() {
         return "Carton is called  " + name + ", costs " + price + ", colored "
-    + color + ",has capacity  " + capacity + ", accounts for " + number;
-}
+                + color + ",has capacity  " + capacity + ", accounts for " + number;
+    }
+
     static void printStaticSum() {
         System.out.println("Overall number is " + generalNumber + " cartons");
     }
+
     public void printSum() {
         System.out.println("Carton is called " + name + ", cartons of milk of this brand " + number + " total number of cartons of milk  " + generalNumber);
     }
-    public void resetValues(String name, double price, String color , double capacity, int number) {
+
+    public void resetValues(String name, double price, String color, double capacity, int number) {
         setName(name);
         setPrice(price);
         setColor(color);
@@ -64,9 +69,11 @@ public class CartonOfMilk {
     public String getColor() {
         return color;
     }
-    public void setColor (String color) {
-    	this.color = color;
+
+    public void setColor(String color) {
+        this.color = color;
     }
+
     public double getCapacity() {
         return capacity;
     }
@@ -75,16 +82,16 @@ public class CartonOfMilk {
         this.capacity = capacity;
     }
 
-  public int getNumber () {
-	  return number;
-  }
+    public int getNumber() {
+        return number;
+    }
 
-    public void setNumber (int number) {
+    public void setNumber(int number) {
         generalNumber -= this.number;
         this.number = number;
         generalNumber += this.number;
     }
-    }
+}
     
     
 
--- forJavaLabs/Lab1/main.java
+++ forJavaLabs/Lab1/main.java
@@ -1,25 +1,25 @@
 
-public class main {
+public class Main {
 
-	public static void main(String[] args) {
-	    CartonOfMilk molokiya = new CartonOfMilk();
-        CartonOfMilk galychyna = new CartonOfMilk ("Galychyna",20.99, "dark blue", 0.9, 1);
-        CartonOfMilk ferma = new CartonOfMilk("Ferma",19.50, "yellow", 1, 23);
-	
-	System.out.println(molokiya.toString());
-    System.out.println(galychyna.toString());
-    System.out.println(ferma.toString());
-    
-    molokiya.resetValues ("Molokiya",23.50 ,"red", 1.5, 5);
+    public static void Main(String[] args) {
+        CartonOfMilk molokiya = new CartonOfMilk();
+        CartonOfMilk galychyna = new CartonOfMilk("Galychyna", 20.99, "dark blue", 0.9, 1);
+        CartonOfMilk ferma = new CartonOfMilk("Ferma", 19.50, "yellow", 1, 23);
 
-    molokiya.printSum();
-    galychyna.printSum();
-    ferma.printSum();
-    
-    molokiya.setNumber(5);
-    
-    CartonOfMilk.printStaticSum();
-	}
+        System.out.println(molokiya.toString());
+        System.out.println(galychyna.toString());
+        System.out.println(ferma.toString());
+
+        molokiya.resetValues("Molokiya", 23.50, "red", 1.5, 5);
+
+        molokiya.printSum();
+        galychyna.printSum();
+        ferma.printSum();
+
+        molokiya.setNumber(5);
+
+        CartonOfMilk.printStaticSum();
+    }
 }
 
 
